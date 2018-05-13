d8f4faf9447669c9489371d66b2b4ef042dc58a5
Noah Berman <noahberm@gmail.com>
2018-05-13 21:56:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ function deleteFromObjectByKey(object, key) {
   return newObj
 }
 
-destructivelyDeleteFromObjectByKey(object, key)
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
